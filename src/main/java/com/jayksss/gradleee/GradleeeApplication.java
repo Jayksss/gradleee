782079f6a307a304f7c1121bdfa198136ab5f6cf
Jayksss <bulnanli@gmail.com>
2022-07-05 15:04:32 +0900

    init
    2022-07-05 ~

---
+++ src/main/java/com/jayksss/gradleee/GradleeeApplication.java
@@ -0,0 +1,13 @@
+package com.jayksss.gradleee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jayksss/gradleee/GradleeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayksss.gradleee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
