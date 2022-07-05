00df71f8b8abac92586663e6777cb4af1b10dc2c
Jayksss <bulnanli@gmail.com>
2022-07-05 15:48:24 +0900

    thymeleaf 반복문
    html에 반복문 적용

--- src/main/java/com/jayksss/gradleee/controller/MainController.java
+++ src/main/java/com/jayksss/gradleee/controller/MainController.java
@@ -1,20 +1,37 @@
 package com.jayksss.gradleee.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
 	@GetMapping("/main")
-	@ResponseBody
-	public String Main(HttpServletRequest request) {
+	public ModelAndView Main(HttpServletRequest request, Model model) {
 		System.out.println("first visit console >>> ");
-		return "first visit";
+		
+		ModelAndView mav = new ModelAndView();
+		List<String> resultList = new ArrayList<String>();
+		
+		resultList.add("AAA");
+		resultList.add("BBB");
+		resultList.add("CCC");
+		resultList.add("DDD");
+		resultList.add("EEE");
+		resultList.add("FFF");
+		
+		mav.addObject("resultList",resultList);
+		mav.setViewName("content/main");
+		
+		return mav;
 	}
 }
